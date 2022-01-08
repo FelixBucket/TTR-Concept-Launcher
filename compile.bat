2de7a75f8118c36118f6683d620480407ef4aa1c
FelixBucket <FelixBucket@users.noreply.github.com>
2022-01-08 15:20:10 -0700

    Update Windows Compiler

--- compile.bat
+++ compile.bat
@@ -1,9 +1,8 @@
 @echo off
 rmdir bin /s /q
-pyinstaller --onefile -i="icon.ico" --noconsole main.py
+pyinstaller --onefile -i="icon.ico" --add-data="resources;resources" --noconsole main.py
 rmdir build /s /q
 REN dist bin
-XCOPY /E /I resources bin\resources\
 cd bin
 REN main.exe Launcher.exe
 PAUSE
