48f7bb8a335c6fc98e27f8ffaaa77528012b0a70
FelixBucket <FelixBucket@users.noreply.github.com>
2022-01-08 15:13:42 -0700

    Update relative path

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ import os
 class Process(multiprocessing.Process):
     pass
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
 def run_launcher(guiToLauncher, launcherToGui):        
     launcher = TTRLauncher(input=guiToLauncher, output=launcherToGui)
     try:
